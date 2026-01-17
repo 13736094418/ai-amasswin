7ddd732a341852144324471ec089066da4a4b430
13736094418 <13736094418@139.com>
2026-01-17 18:57:01 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    outDir: 'dist',
-    sourcemap: true
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://api.ai.amasswin.com', // 修改这里
+        changeOrigin: true
+      }
+    }
   }
 })
