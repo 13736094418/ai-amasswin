cb2f22999e79614ad9d6840cb49bd0545109d6f2
13736094418 <13736094418@139.com>
2026-01-17 18:55:52 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
+  build: {
+    outDir: 'dist',
+    sourcemap: true
   }
 })
